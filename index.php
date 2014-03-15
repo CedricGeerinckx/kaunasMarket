6585b1cd66279489178ce9f4a2131808ecc3ca38
Smokevision <posti@smokevision.fi>
2014-03-15 17:16:41 +0200

    update
    updated pretty much everything

--- index.php
+++ index.php
@@ -4,16 +4,26 @@
 <title>The Kaunas Market</title>
 
 <link rel="stylesheet" type="text/css" href="css/jquery.fullPage.css" />
-<link rel="stylesheet" type="text/css" href="css/all.css" />
+<link rel="stylesheet" type="text/css" href="css/main.css" />
 
 </head>
 
 <body>
-
-	<div class="section active">Some section</div>
-	<div class="section"><h1>Some section</h1></div>
-	<div class="section">Some section</div>
-	<div class="section">Some section</div>
+	<div id="navigation">
+		
+	</div>
+	<div class="section active" id="home">
+		<h1>Home</h1>
+	</div>
+	<div class="section" id="info">
+		<h1>Info</h1>
+	</div>
+	<div class="section" id="map">
+		<h1>Map</h1>
+	</div>
+	<div class="section" id="history">
+		<h1>History</h1>
+	</div>
 
 <script src="js/jquery.js"></script>
 <script src="js/jquery-ui.js"></script>
