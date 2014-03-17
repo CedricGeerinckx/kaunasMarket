6623e69354a6db3fab9dd317bf4bb4e5512ea814
Smokevision <posti@smokevision.fi>
2014-03-17 16:22:12 +0200

    added map
    added map with 10 markers

--- index.php
+++ index.php
@@ -6,11 +6,12 @@
 <link rel="stylesheet" type="text/css" href="css/jquery.fullPage.css" />
 <link rel="stylesheet" type="text/css" href="css/main.css" />
 
-
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>	
 <script src="js/jquery.fullPage.min.js"></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAnnddgOPyxcKNLuayGpUFqmrNAVbusnk8&sensor=true"></script>
 <script src="js/main.js"></script>
+<script src="js/map.js"></script>
 
 </head>
 
@@ -27,7 +28,8 @@
 		<h1>Info</h1>
 	</div>
 	<div class="section" id="section2">
-		<h1>Map</h1>
+		<div id="map">
+		</div>
 	</div>
 	<div class="section" id="section3">
 		<h1>History</h1>
