096467acc07383597d84decea230a0add6c4cd51
Smokevision <posti@smokevision.fi>
2014-03-17 12:13:11 +0200

    design sketch, login, database
    design sketch in the root

--- index.php
+++ index.php
@@ -14,6 +14,11 @@
 	</div>
 	<div class="section active" id="home">
 		<h1>Home</h1>
+		<form>
+            <textarea name="editor1" id="editor1" rows="10" cols="80">
+                This is my textarea to be replaced with CKEditor.
+            </textarea>
+        </form>
 	</div>
 	<div class="section" id="info">
 		<h1>Info</h1>
@@ -29,7 +34,6 @@
 <script src="js/jquery-ui.js"></script>
 <script src="js/jquery.fullPage.min.js"></script>
 <script src="js/main.js"></script>
-
 </body>
 
 </html>
--- login.php
+++ login.php
@@ -1,22 +1,42 @@
 <?php
 session_start();
 if(isset($_REQUEST["logout"])){
-	session_unset();
+	session_destroy();
 }
-elseif(isset($_SESSION['loginstatus']) && $_SESSION['loginstatus']=="logged"){
-	//header("Location: index.php");
-	echo "<a href='login.php?logout'>Logout</a>";
+elseif(isset($_SESSION['LOGIN_STATUS']) && $_SESSION['LOGIN_STATUS']=="logged"){
+	header("Location: index.php");
 }
 elseif(isset($_POST['button'])){
-	if(empty($_POST['user']) || empty($_POST['user'])){
-		echo "Username or password is incorrect";
+	include("includes/functions.php");
+	if(empty($_POST['user']) || empty($_POST['pass'])){
+		echo "You must enter username and password.";
 	}
 	else{
-		$_SESSION['loginstatus']="logged";
-		header("Location: index.php");
+		$user = $_POST["user"];
+		$pass = md5($_POST["pass"]);
+		$loginstatus = login($user, $pass);
+		if($loginstatus){
+			header("Location: index.php");
+		}
 	}
 }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+<meta charset="UTF-8"/>
+<link rel="stylesheet" type="text/css" href="css/login.css" />
+</head>
+
+<body>
+<?php
+if(isset($_SESSION["ERRMSG"])){
+	echo $_SESSION["ERRMSG"];
+	unset($_SESSION["ERRMSG"]);
+}
+?>
+<div id="container">
 <h1>Login</h1>
 <form method="POST" action="<?php echo $_SERVER['PHP_SELF']?>">
 	<p>Username:<br/>
@@ -26,3 +46,6 @@ elseif(isset($_POST['button'])){
 	</p>
 	<input type="submit" name="button" value="Login"/>
 </form>
+</div>
+</body>
+</html>
