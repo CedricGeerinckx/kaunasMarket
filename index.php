1d36a1be9dee3177f9389bb3e7d8dcb87f6dedb4
Cedric Geerinckx <Cedric.geerinckx@hotmail.com>
2014-03-17 13:26:05 +0100

    fullpage fix

--- index.php
+++ index.php
@@ -6,13 +6,18 @@
 <link rel="stylesheet" type="text/css" href="css/jquery.fullPage.css" />
 <link rel="stylesheet" type="text/css" href="css/main.css" />
 
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>	
+<script src="js/jquery.fullPage.min.js"></script>
+<script src="js/main.js"></script>
+
 </head>
 
 <body>
 	<div id="navigation">
 		
 	</div>
-	<div class="section active" id="home">
+	<div class="section" id="home">
 		<h1>Home</h1>
 	</div>
 	<div class="section" id="info">
@@ -25,10 +30,6 @@
 		<h1>History</h1>
 	</div>
 
-<script src="js/jquery.js"></script>
-<script src="js/jquery-ui.js"></script>
-<script src="js/jquery.fullPage.min.js"></script>
-<script src="js/main.js"></script>
 
 </body>
 
